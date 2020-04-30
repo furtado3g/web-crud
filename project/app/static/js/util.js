269015e88dfb67ba3544dacf5dc5abe35ebb50b4
Furtado3g <lucas_shiguioka|@hotmail.com>
2020-04-30 12:55:31 -0300

    finished exam

--- project/app/static/js/util.js
+++ project/app/static/js/util.js
@@ -1,12 +1,38 @@
 async function abreProduto(id){
-    const url = '';
+    const url = '/atualizar/';
     const param = id;
     const options = {
-        method : "post"
+        method : "get"
     }
 
-    await fetch(url+params)
+    await fetch(url+params,options)
     .then(response=>{
-        
+        return response.text()
+    })
+    .then(data=>{
+        document.querySelector("#modal-container").removeChild()
+        document.querySelector("#modal-container").innerHTML = data
+    })
+    .catch(e=>{
+        console.log(e)
+    })
+}
+
+async function excluirProduto(id){
+    const url = '/atualizar/';
+    const param = id;
+    const options = {
+        method : "delete"
+    }
+
+    await fetch(url+params,options)
+    .then(response=>{
+        return response.text()
+    })
+    .then(data=>{
+        location.reload()
+    })
+    .catch(e=>{
+        console.log(e)
     })
 }
--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import produtos
+from django.forms import ModelForm
+from django.http import Http404,JsonResponse
+from django.views.decorators.csrf import csrf_protect,ensure_csrf_cookie,csrf_exempt
 # Create your views here.
+class ProductForm(ModelForm):
+    class Meta:
+        model = produtos
+        fields = ['nome','cod_barras','valor']
+
+@csrf_exempt
 def home(request):
     if(request.method == "GET"):
         try:
@@ -11,3 +20,40 @@ def home(request):
             'listaDeProdutos' : Produtos
         }
         return render(request,'home.html',context)
+    if(request.method == 'POST'):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(request,'/')
+        else:
+            context = {
+                'erros' : 'Erro ao preencher'
+            }
+            return render(request,'home.html',context)
+
+@csrf_exempt
+def atualizar(request,pk):
+    if request.method == 'GET' :
+        try:
+            produto = produtos.objects.filter(pk=pk)
+        except:
+            raise Http404("forbiden page")
+        context = {
+            'produtos' : produto
+        }
+        return render(request,'modalProduto.html',context)
+    elif request.method == 'POST' :
+        user= get_object_or_404(User, pk=pk) 
+        form = UserForm(request.POST,instance=user)
+        if form.is_valid():
+            form.save( )
+            return redirect(request,'/')
+        else:
+            context = {
+                'erros' : 'Erro ao editar produto'
+            }
+            return render(request,'home.html',context)
+    elif request.method == "DELETE":
+        User.objects.filter(pk).delete()
+        return redirect(request,'/')
+
