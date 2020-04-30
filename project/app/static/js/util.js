315460db6ac839c30bf2a69627e4f5a4721771a4
Furtado3g <lucas_shiguioka|@hotmail.com>
2020-04-29 23:44:28 -0300

    finished base template

---
+++ project/app/static/js/util.js
@@ -0,0 +1,12 @@
+async function abreProduto(id){
+    const url = '';
+    const param = id;
+    const options = {
+        method : "post"
+    }
+
+    await fetch(url+params)
+    .then(response=>{
+        
+    })
+}
--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import produtos
 # Create your views here.
+def home(request):
+    if(request.method == "GET"):
+        try:
+            Produtos = produtos.objects.all()
+        except:
+            raise Http404("forbiden page")
+        context = {
+            'listaDeProdutos' : Produtos
+        }
+        return render(request,'home.html',context)
